1d81ee5944833fee87cffbf265c1cbd34f58605a
paukshto <dr.tvn@yandex.ru>
2022-11-02 19:36:26 +0300

    minimal server

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+
+	router := chi.NewRouter()
+	router.Use(middleware.Compress(5))
+
+	// router.Use(AuthMw)
+	// router.Use(middleware.Compress(5))
+	log.Panic(http.ListenAndServe("/", router))
+}
