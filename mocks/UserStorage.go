46352c4ce3949bf9e7234aeee8d7a67a988afac1
paukshto <dr.tvn@yandex.ru>
2022-12-04 23:10:45 +0300

    fix tests (+govet)

--- mocks/UserStorage.go
+++ mocks/UserStorage.go
@@ -98,13 +98,13 @@ func (_m *UserStorage) GetUser(_a0 context.Context, _a1 sharedtypes.Credentials)
 	return r0, r1
 }
 
-// UpdateUser provides a mock function with given fields: _a0, _a1, _a2
-func (_m *UserStorage) UpdateUser(_a0 context.Context, _a1 string, _a2 float32) error {
-	ret := _m.Called(_a0, _a1, _a2)
+// UpdateUser provides a mock function with given fields: _a0, _a1, _a2, _a3
+func (_m *UserStorage) UpdateUser(_a0 context.Context, _a1 string, _a2 string, _a3 float32) error {
+	ret := _m.Called(_a0, _a1, _a2, _a3)
 
 	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, string, float32) error); ok {
-		r0 = rf(_a0, _a1, _a2)
+	if rf, ok := ret.Get(0).(func(context.Context, string, string, float32) error); ok {
+		r0 = rf(_a0, _a1, _a2, _a3)
 	} else {
 		r0 = ret.Error(0)
 	}
